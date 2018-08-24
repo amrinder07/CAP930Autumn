1fa232b635750879c90aba3ede599697c069e7ab
amrinder07 <41987278+amrinder07@users.noreply.github.com>
2018-08-24 14:04:35 +0530

    Add files via upload

---
+++ eval1.py
@@ -0,0 +1,22 @@
+print("Employee Basic Details: ")
+employee={"Emp_id":101,"Emp_name":"Amrinder","Contact_no":9875642135,"Email_id":"amrinder@gmail.com","DOB":"07-12-1995","Gender":"Male"}
+for i in employee.values():
+    print(i)
+    
+print("\n")
+print("Employee' Qualifications: ")
+qualifications=("HSC: CBSE, 7.20","SSC: CBSE, 75.80","Under_Graduation: Bachelor of Computer Applications","Post_Graduation: Master of Computer Application")
+for i in qualifications:
+    print(i)
+    
+print("\n")
+print("Technical Skills: ")
+Technical_Skills=["C","C++","Java","PHP","HTML","Swift Programming","Android Development","Python","Ruby on Rails"]
+for i in Technical_Skills:
+    print(i)
+    
+print("\n")
+print("Other Skills: ")
+Other_Skills=["Good Communication Skills","Multi-Tasking","Good Listener","Intellectual Thinking"]
+for i in Other_Skills:
+    print(i)
